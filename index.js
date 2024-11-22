fe844e9a6d0e231c94fc349436e4a07450748be5
Ted <tedshd@gmail.com>
2024-11-22 15:24:56 +0800

    Update index.js

--- index.js
+++ index.js
@@ -8,22 +8,32 @@
     var list = dom.querySelectorAll("li");
     dom
       .querySelector(".drop_menu_title")
-      .addEventListener("click", function () {
+      .addEventListener("click", function (e) {
+        e.stopPropagation();
         ul.classList.toggle("hide");
       });
     for (var i = 0; i < list.length; i++) {
       var element = list[i];
-      element.addEventListener("click", function () {
-        console.log("click");
+      element.addEventListener("click", function (e) {
+        e.stopPropagation();
         if (ul.querySelector(".active")) {
           ul.querySelector(".active").classList.remove("active");
         }
         this.classList.add("active");
+        dom.querySelector(".drop_menu_title span").innerText =
+          this.innerText.trim();
         if (callback) {
           callback(this);
         }
+        ul.classList.toggle("hide");
       });
     }
+    window.addEventListener("click", function (e) {
+      e.stopPropagation();
+      if (!ul.classList.contains("hide")) {
+        ul.classList.add("hide");
+      }
+    });
   };
 
   window.dropMenu = dropMenu;
